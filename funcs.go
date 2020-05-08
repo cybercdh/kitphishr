c68366b5c6814d66dbd4abde4747371377f516ed
cybercdh <cdh.cert@gmail.com>
2020-05-08 08:45:22 +0100

    v0.1

--- funcs.go
+++ funcs.go
@@ -3,32 +3,36 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"crypto/sha1"
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	termutil "github.com/andrew-d/go-termutil"
+	"io"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"net/url"
 	"os"
+	"path"
 	"strings"
 	"time"
 )
 
-const MAX_DOWNLOAD_SIZE = 104857600 // 100MB
-
-// custom struct for parshing phishtank urls
+// custom struct for parsing phishtank urls
 type PhishUrls struct {
 	URL string `json:"url"`
 }
 
-// read from the PhishTank URL and return just the urls
+/*
+	read from the PhishTank URL and return just the urls
+*/
 func GetPhishTankURLs() ([]PhishUrls, error) {
 
 	pturl := "http://data.phishtank.com/data/online-valid.json"
 
+	// if the user has their own phishtank api key, use it
 	apiKey := os.Getenv("PT_API_KEY")
 	if apiKey != "" {
 		pturl = fmt.Sprintf("http://data.phishtank.com/data/%s/online-valid.json", apiKey)
@@ -51,18 +55,21 @@ func GetPhishTankURLs() ([]PhishUrls, error) {
 	return urls, nil
 }
 
-// process user input
-func GetUserInput() chan string {
-	var urls []PhishUrls
+/*
+	get a list of urls either from the user
+	piping into this program, or fetch the latest
+	phishing urls from phishtank
+*/
+func GetUserInput() ([]PhishUrls, error) {
 
-	_urls := make(chan string, 1)
+	var urls []PhishUrls
 
 	// if nothing on stdin, default getting input from phishtank
 	if termutil.Isatty(os.Stdin.Fd()) {
 
 		pturls, err := GetPhishTankURLs()
 		if err != nil {
-			panic(err)
+			return urls, err
 		}
 		urls = pturls
 
@@ -75,16 +82,25 @@ func GetUserInput() chan string {
 		}
 	}
 
-	/*
-	   now we have our urls
-	   iterate through the paths in each to generate
-	   a target list...e.g.
-	     http://example.com/foo/bar
-	     http://example.com/foo/
-	     http://example.com/
-	   and feed them to the _urls channel to process
-	*/
+	return urls, nil
+
+}
+
+/*
+   iterate through the paths of each url to generate
+   a target list...e.g.
+     http://example.com/foo/bar
+     http://example.com/foo/bar.zip
+     http://example.com/foo/
+     http://example.com/foo.zip
+     http://example.com/
+*/
+func GenerateTargets(urls []PhishUrls) chan string {
+
+	_urls := make(chan string, 1)
+
 	go func() {
+
 		seen := make(map[string]bool)
 
 		for _, row := range urls {
@@ -93,9 +109,6 @@ func GetUserInput() chan string {
 			// parse the url
 			u, err := url.Parse(myurl)
 			if err != nil {
-				if verbose {
-					fmt.Printf("[!] Error processing %s\n", myurl)
-				}
 				continue
 			}
 			// split the paths from the parsed url
@@ -116,122 +129,147 @@ func GetUserInput() chan string {
 
 				// feed the _urls channels
 				_urls <- tmp_url
+
+				// guess zip path and send to targets
+				zipurl := tmp_url + ".zip"
+
+				// ignore http://example.com/.zip and http://example.com.zip
+				if strings.HasSuffix(zipurl, "/.zip") || strings.Count(zipurl, "/") < 3 {
+					continue
+				}
+
+				// add this one to seen too
+				seen[zipurl] = true
+
+				// feed the _urls channels
+				_urls <- zipurl
 			}
 		}
 		close(_urls)
 	}()
+
 	return _urls
 }
 
-func IsPathAZip(client *http.Client, url string) bool {
+func ZipFromDir(resp *http.Response) (string, error) {
 
-	req, err := http.NewRequest("HEAD", url, nil)
-	if err != nil {
-		return false
-	}
-
-	req.Header.Add("Connection", "close")
-	req.Close = true
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return false
-	}
-
-	contentType := resp.Header.Get("Content-Type")
-
-	if contentlength > 0 && strings.Contains(contentType, "zip") {
-		return true
-	}
-
-	return false
-}
-
-func ZipFromDir(client *http.Client, url string) string {
-
-	// perform a GET
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return ""
-	}
-
-	req.Header.Add("Connection", "close")
-	req.Close = true
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return ""
-	}
-
-	defer resp.Body.Close()
-
-	zdurl := ""
-
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return ""
-		}
-		bodyString := string(bodyBytes)
-		if !strings.Contains(bodyString, "Index Of /") {
-			return ""
-		}
-	}
+	ziphref := ""
 
 	// read body for hrefs
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
-		return ""
+		return ziphref, err
 	}
 
-	// todo - crawl folders to find more zips
-	doc.Find("a").Each(func(i int, s *goquery.Selection) {
-		href, ok := s.Attr("href")
-		if ok {
-			if strings.Contains(href, ".zip") {
-				if strings.HasSuffix(url, "/") {
-					zdurl = url + href
-				} else {
-					zdurl = url + "/" + href
-				}
-			}
-		}
-	})
+	title := doc.Find("title").Text()
 
-	return zdurl
+	if strings.Contains(title, "Index of /") {
+		doc.Find("a").Each(func(i int, s *goquery.Selection) {
+			if strings.Contains(s.Text(), ".zip") {
+				ziphref = s.Text()
+			}
+		})
+	}
+
+	return ziphref, nil
 }
 
+/*
+	make an http client
+	allow redirects
+	skip ssl warnings
+	set some timeouts
+*/
 func MakeClient() *http.Client {
 
 	proxyURL := http.ProxyFromEnvironment
 
 	var tr = &http.Transport{
 		Proxy:               proxyURL,
-		MaxIdleConns:        1000,
-		MaxIdleConnsPerHost: 500,
-		MaxConnsPerHost:     500,
-		IdleConnTimeout:     time.Second,
+		// MaxIdleConns:        1000,
+		// MaxIdleConnsPerHost: 500,
+		// MaxConnsPerHost:     500,
+		// IdleConnTimeout:     time.Second * 1,
 		DisableKeepAlives:   true,
 		TLSClientConfig: &tls.Config{
 			InsecureSkipVerify: true,
 			Renegotiation:      tls.RenegotiateOnceAsClient,
 		},
 		DialContext: (&net.Dialer{
-			Timeout:   time.Second * 5,
-			KeepAlive: time.Second,
+			Timeout: 30 * time.Second,
+			// KeepAlive: time.Second,
+			DualStack: true,
 		}).DialContext,
 	}
 
-	re := func(req *http.Request, via []*http.Request) error {
-		return http.ErrUseLastResponse
-	}
+	// re := func(req *http.Request, via []*http.Request) error {
+	// 	return http.ErrUseLastResponse
+	// }
 
 	client := &http.Client{
-		Transport:     tr,
-		CheckRedirect: re,
-		Timeout:       time.Second * 5,
+		Transport: tr,
+		// CheckRedirect: re,
+		Timeout: time.Second * 15,
 	}
 
 	return client
 
+}
+
+/*
+	peform a GET against the target URL
+	return the response
+*/
+func AttemptTarget(client *http.Client, url string) (*http.Response, error) {
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
+	req.Header.Add("Connection", "close")
+	req.Close = true
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+
+}
+
+/*
+	saves the resp.body to a file
+	calls it name.ext_sha1
+	returns name of file, err
+*/
+func SaveResponse(resp *http.Response) (string, error) {
+
+	filename := ""
+
+	// convert the body to bytes to generate the sha1sum
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return filename, err
+	}
+
+	data := []byte(bodyBytes)
+	checksum := sha1.Sum(data)
+	filename = fmt.Sprintf("%s_%x", path.Base(resp.Request.URL.Path), checksum)
+
+	// restore the ioreader to get back the content of resp.body
+	resp.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+
+	// create the output file
+	out, err := os.Create(defaultOutputDir + "/" + filename)
+	if err != nil {
+		return filename, err
+	}
+	defer out.Close()
+
+	// write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return filename, err
 }
--- main.go
+++ main.go
@@ -4,97 +4,195 @@ import (
 	"flag"
 	"fmt"
 	"github.com/gookit/color"
+	"net/http"
+	"os"
 	"strings"
 	"sync"
 )
 
-const (
-	defaultOutputDir = "./kits"
-)
+const MAX_DOWNLOAD_SIZE = 104857600 // 100MB
 
-// globals arg vars
 var verbose bool
+var downloadKits bool
 var concurrency int
+var defaultOutputDir string
 
 func main() {
 
-	flag.IntVar(&concurrency, "c", 20, "set the concurrency level")
-	flag.BoolVar(&verbose, "v", false, "Get more info on URL attempts")
+	flag.IntVar(&concurrency, "c", 50, "set the concurrency level")
+	flag.BoolVar(&verbose, "v", false, "get more info on URL attempts")
+	flag.BoolVar(&downloadKits, "d", false, "option to download suspected phishing kits")
+	flag.StringVar(&defaultOutputDir, "o", "kits", "directory to save output files")
+
 	flag.Parse()
 
 	client := MakeClient()
 
-	zips := make(chan string)
-	dirs := make(chan string)
+	targets := make(chan string)
+	responses := make(chan *http.Response)
+	tosave := make(chan *http.Response)
 
+	// create the output directory, ready to save files to
+	if downloadKits {
+		err := os.MkdirAll(defaultOutputDir, os.ModePerm)
+		if err != nil {
+			fmt.Printf("There was an error creating the output directory : %s\n", err)
+			os.Exit(1)
+		}
+	}
+
+	// worker group to fetch the urls from targets channel
+	// send the output to responses channel for further processing
 	var wg sync.WaitGroup
 
-	for i := 0; i < concurrency; i++ {
+	for i := 0; i < concurrency/2; i++ {
 
-		wg.Add(2)
+		wg.Add(1)
 
 		go func() {
+
 			defer wg.Done()
 
-			for url := range zips {
-
+			for url := range targets {
 				if verbose {
 					fmt.Printf("Attempting %s\n", url)
 				}
+				res, err := AttemptTarget(client, url)
+				if err != nil {
+					continue
+				}
 
-				if IsPathAZip(client, url) {
-					if verbose {
-						color.Green.Printf("Found phishing kit from path at %s\n", url)
+				responses <- res
+			}
+
+		}()
+
+	}
+
+	// response group
+	// determines if we've found a zip from a url folder
+	// or if we've found an open directory and looks for a zip within
+	var rg sync.WaitGroup
+	for i := 0; i < concurrency/2; i++ {
+		rg.Add(1)
+
+		go func() {
+
+			defer rg.Done()
+
+			for resp := range responses {
+
+				if resp.StatusCode != http.StatusOK {
+					continue
+				}
+
+				defer resp.Body.Close()
+
+				requrl := resp.Request.URL.String()
+
+				// if we found a zip from a URL path
+				if strings.HasSuffix(requrl, ".zip") {
+
+					contentlength := resp.ContentLength
+					contentType := resp.Header.Get("Content-Type")
+
+					// make sure it's a valid zip
+					if contentlength > 0 && contentlength < MAX_DOWNLOAD_SIZE && strings.Contains(contentType, "zip") {
+
+						if verbose {
+							color.Green.Printf("Zip found from URL folder at %s\n", requrl)
+						} else {
+							fmt.Println(requrl)
+						}
+
+						// download the zip
+						if downloadKits {
+							tosave <- resp
+							continue
+						}
+					}
+				}
+
+				// todo - if resp contains index of, parse the links
+				href, err := ZipFromDir(resp)
+				if err != nil {
+					continue
+				}
+				if href != "" {
+					hurl := ""
+					if strings.HasSuffix(requrl, "/") {
+						hurl = requrl + href
 					} else {
-						fmt.Println(url)
+						hurl = requrl + "/" + href
+					}
+					if verbose {
+						color.Green.Printf("Zip found from Open Directory at %s\n", hurl)
+					} else {
+						fmt.Println(hurl)
+					}
+					if downloadKits {
+						resp, err := AttemptTarget(client, hurl)
+						if err != nil {
+							if verbose {
+								color.Red.Printf("There was an error downloading %s\n", hurl)
+							}
+							continue
+						}
+						tosave <- resp
+						continue
 					}
 				}
 			}
 		}()
+	}
+
+	// save group
+	var sg sync.WaitGroup
+
+	// give is a few threads to play with
+	for i := 0; i < 10; i++ {
+
+		sg.Add(1)
 
 		go func() {
-			defer wg.Done()
-
-			for url := range dirs {
-				if verbose {
-					fmt.Printf("Checking for open directory at %s\n", url)
-				}
-
-				zdurl := ZipFromDir(client, url)
-				if zdurl == "" {
+			defer sg.Done()
+			for resp := range tosave {
+				filename, err := SaveResponse(resp)
+				if err != nil {
+					color.Red.Printf("There was an error saving %s : %s\n", resp.Request.URL.String(), err)
 					continue
-				}
-				if verbose {
-					color.Green.Printf("Found a zip from open directory at %s\n", zdurl)
 				} else {
-					fmt.Println(zdurl)
+					if verbose {
+						color.Yellow.Printf("Successfully saved %s\n", filename)
+					}
 				}
 			}
 		}()
-
-	} // concurrency
-
-	// get input eithe from user or phishtank
-	urls := GetUserInput()
-
-	for url := range urls {
-
-		// send to dirs channel
-		dirs <- url
-
-		// prep and send to zips channel
-		zipurl := url + ".zip"
-
-		// ignore http://example.com/.zip
-		// ignore http://example.com.zip
-		if strings.HasSuffix(zipurl, "/.zip") || strings.Count(zipurl, "/") < 3 {
-			continue
-		}
-
-		zips <- zipurl
 	}
 
-	close(zips)
-	close(dirs)
+	// get input either from user or phishtank
+	input, err := GetUserInput()
+	if err != nil {
+		fmt.Printf("There was an error getting URLS from PhishTank.\n")
+		os.Exit(3)
+	}
+
+	// generate targets based on user input
+	urls := GenerateTargets(input)
+
+	// send target urls to target channel
+	for url := range urls {
+		targets <- url
+	}
+
+	// netflix and chill.
+	close(targets)
 	wg.Wait()
+
+	close(responses)
+	rg.Wait()
+
+	close(tosave)
+	sg.Wait()
+
 }
